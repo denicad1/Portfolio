78a33d514bf67ee76384131c9697b4b1a40e7b23
anthony <denicad@msn.com>
2023-03-12 19:02:57 -0400

    sidebar almost done. need font to grow

--- src/index.js
+++ src/index.js
@@ -11,20 +11,21 @@
 //changes header/sidebar size, moves image and clears links
   function headerResize(change) {
     const header=document.getElementsByTagName("header")[0];
-    console.log(header);
     if (!change) {
       headerWidth(header,"60","20");
+
       
     } else {
       headerWidth(header,"20","60");
       imgResize();
+      
     }
     
   }
   //change header width
   function headerWidth(ele,size,size1) {
     ele.classList.remove(`w-${size}`);
-    ele.classList.add(`w-${size1}`);
+    ele.classList.add(`w-${size1}`); 
     
   }
   //resize headerImg in header. still needs to be worked on. transition not working and overflow hidden removing most of image. 
@@ -32,8 +33,15 @@
   function imgResize() {
    const headerImg=document.getElementById("headerImage");
     const img=document.getElementById("image");
-   console.log(headerImg);
-  //  headerImg.classList.toggle("-right-[50%]");
-  //  img.classList.toggle("border-emerald-300","border-4");
 
   }
+  //this function might be useful later
+  // function showText() {
+  //   const texts=document.getElementsByClassName("added-text");
+  //   const arrayTexts=[...texts];
+    
+  //   arrayTexts.forEach(ele => {
+  //     ele.classList.toggle("hideText"); 
+  //     ele.classList.toggle("showText");
+  //   });
+  // }
